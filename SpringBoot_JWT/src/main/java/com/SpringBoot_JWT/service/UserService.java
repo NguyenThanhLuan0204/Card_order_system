9d83f9c112695278f6b6fd5c646d6b2d038684f8
Nguyễn Thành Luân <nguyenthanhluantqd@gmail.com>
2023-08-03 22:28:12 +0700

    Revert "module create jwt"
    This reverts commit 7041851a7cdcb47221b652fe298d883349f9da22.

--- SpringBoot_JWT/src/main/java/com/SpringBoot_JWT/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBoot_JWT.service;
-
-import com.SpringBoot_JWT.authen.UserPrincipal;
-import com.SpringBoot_JWT.entity.User;
-
-public interface UserService {
-    User createUser(User user);
-    UserPrincipal findByUsername(String username);
-}
